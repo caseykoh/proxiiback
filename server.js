b52f0653f09c7072953e640d245f7ef08f0c9ece
Casey Koh <s.casey.koh@gmail.com>
2024-07-18 10:45:43 -0400

    Remove email router

--- server.js
+++ server.js
@@ -23,7 +23,6 @@ app.get("/", (req, res) => {
 
 app.use("/appointments", appointmentsRouter);
 app.use("/imageurls", imageUrlsRouter);
-app.use("/imageurls", emailRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
