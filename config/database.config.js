2245fe0266e3187860b8fcf30ddae10bf417f994
Casey Koh <s.casey.koh@gmail.com>
2024-07-18 10:41:03 -0400

    Add config for test and production

--- config/database.config.js
+++ config/database.config.js
@@ -12,18 +12,18 @@ module.exports = {
     host: DEV_DATABASE_HOST,
     dialect: "postgres",
   },
-  // test: {
-  //   username: "root",
-  //   password: null,
-  //   database: "database_test",
-  //   host: "127.0.0.1",
-  //   dialect: "postgres",
-  // },
-  // production: {
-  //   username: "root",
-  //   password: null,
-  //   database: "database_production",
-  //   host: "127.0.0.1",
-  //   dialect: "postgres",
-  // },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "postgres",
+  },
+  production: {
+    username: "root",
+    password: null,
+    database: "database_production",
+    host: "127.0.0.1",
+    dialect: "postgres",
+  },
 };
