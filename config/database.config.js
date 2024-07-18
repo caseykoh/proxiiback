6bbad6bb2d1fdd7249a8a8ee42b40e054c1b2373
Casey Koh <s.casey.koh@gmail.com>
2024-07-18 11:00:55 -0400

    Update prod db name

--- config/database.config.js
+++ config/database.config.js
@@ -15,14 +15,14 @@ module.exports = {
   test: {
     username: "root",
     password: null,
-    database: "database_test",
+    database: "proxiitest-db",
     host: "127.0.0.1",
     dialect: "postgres",
   },
   production: {
     username: "root",
     password: null,
-    database: "database_production",
+    database: "proxiidb",
     host: "127.0.0.1",
     dialect: "postgres",
   },
